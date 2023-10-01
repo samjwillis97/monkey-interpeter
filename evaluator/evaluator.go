a65e4ae28c61eb076a67908979df41c27856f6b3
Sam Willis <sam@williscloud.org>
2023-10-01 15:26:45 +1100

    if-else done

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -27,6 +27,11 @@ func Eval(node ast.Node) object.Object {
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 
 		// Expressions
 	case *ast.Boolean:
@@ -126,5 +131,30 @@ func evalIntegerInfixExpression(operator string, left object.Object, right objec
 		return nativeBooltoBooleanObject(leftValue != rightValue)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
